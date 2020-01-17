fc2d4f35d87c6110079f46c4087687c18cb2ca9c
Tomer Kizel <Tomerkizel@gmail.com>
2020-01-17 18:28:37 +0200

    Initial Commit

---
+++ ios_example_girlad/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios_example_girlad
+//
+//  Created by Apple on 17/01/2020.
+//  Copyright © 2020 DontBeAStranger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
